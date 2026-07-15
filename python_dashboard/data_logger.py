ef0cbbaaa7371a54b258552506390e3820f2f91d
Human507 <70184379+Human507@users.noreply.github.com>
2026-07-15 18:00:57 -0500

    Added basic log_data class structure

--- python_dashboard/data_logger.py
+++ python_dashboard/data_logger.py
@@ -1,4 +1,7 @@
 import csv
-def log_data():
-    #this function will add the data to the csv
-    print("hi")
+class log_data:
+    def __init__(self, filename):
+        self.filename = filename
+        #self.file = ...
+        #self.writer = ...
+        self.header_written = False
