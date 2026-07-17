fcd0ff1a219c4173baf775d06599dec7a45b0e59
Human507 <70184379+Human507@users.noreply.github.com>
2026-07-17 15:56:15 -0500

    Added header to DataLogger class and also added file closing for ctrl+c

--- python_dashboard/data_logger.py
+++ python_dashboard/data_logger.py
@@ -1,11 +1,15 @@
 import csv
 class DataLogger:
-    def __init__(self, filename):
+    def __init__(self, filename, header):
         self.filename = filename
         self.file = open(filename, 'w', newline='')
         self.writer = csv.writer(self.file)
         self.header_written = False
+        self.header = header
     def log(self, content):
+        if self.header_written == False:
+            self.writer.writerow(self.header)
+            self.header_written = True
         self.writer.writerow(content)
         self.file.flush()
     def close(self):
--- python_dashboard/main.py
+++ python_dashboard/main.py
@@ -4,16 +4,17 @@ from data_logger import DataLogger
 
 def main():
     sensor = DataGenerator()
-    logger = DataLogger("log.csv")
+    logger = DataLogger("log.csv", ['Time', 'Value'])
     start_time = time.time()
+    try:
+        while True:
+            t = time.time() - start_time
+            value = sensor.read()
+            logger.log([t, value])
+            #print(f"t={t:.2f}, value={value:.2f}")
 
-    while True:
-        t = time.time() - start_time
-        value = sensor.read()
-        logger.log([t, value])
-        #print(f"t={t:.2f}, value={value:.2f}")
-
-        time.sleep(0.1)  # simulates sampling rate (10 Hz)
-
+            time.sleep(0.1)  # simulates sampling rate (10 Hz)
+    finally:
+        logger.close()
 if __name__ == "__main__":
     main()
